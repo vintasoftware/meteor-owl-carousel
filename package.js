e536d6ef12ee9451fad93c238f52f2204f09d247
sme001 <sme001@www.sme001.com>
2015-06-24 03:17:27 +0800

    upgrade to 2.0.0-beta.3

--- package.js
+++ package.js
@@ -1,5 +1,7 @@
 Package.describe({
-  summary: 'Touch enabled jQuery plugin that lets you create a beautiful responsive carousel slider.'
+  name: 'owl-carousel',
+  summary: 'Touch enabled jQuery plugin that lets you create a beautiful responsive carousel slider.',
+  version: '0.1.2'
 });
 
 Package.on_use(function (api) {
